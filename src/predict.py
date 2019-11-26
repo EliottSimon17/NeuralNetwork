53e2e2c59afb6dd01181da5e69770a7859202ec7
EliottSimon17 <elili2000@gmail.com>
2019-11-26 22:50:23 +0100

    Initial commit

---
+++ src/predict.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+from sigmoid import sigmoid
+
+def predict(Theta1, Theta2, X):
+#PREDICT Predict the label of an input given a trained neural network
+#   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
+#   trained weights of a neural network (Theta1, Theta2)
+
+# Useful values
+    m = np.shape(X)[0]              #number of examples
+    
+# You need to return the following variables correctly 
+    p = np.zeros(m);
+
+# ====================== YOUR CODE HERE ======================
+# Instructions: Complete the following code to make predictions using
+#               your learned neural network. You should set p to a 
+#               vector containing labels between 1 to num_labels.
+#
+# Hint: The max function might come in useful. In particular, the max
+#       function can also return the index of the max element, for more
+#       information see 'help max'. If your examples are in rows, then, you
+#       can use max(A, [], 2) to obtain the max for each row.
+#
+
+
+
+    return p
+
+# =========================================================================
